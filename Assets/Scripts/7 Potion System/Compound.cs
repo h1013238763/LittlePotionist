65204b2b97a3c5e27856a6b29c3744a7ed3cd1a7
1013238763@qq.com <1013238763@qq.com>
2023-06-10 21:39:47 -0700

    problems...

--- Assets/Scripts/7 Potion System/Compound.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-public class Compound
-{
-    public int material_id;         // id of Material
-    public string material_name;    // name of Material
-    public float material_num;      // num of Material
-}
